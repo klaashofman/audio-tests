1cb907b246b434b04024407950678c378f3a3094
Klaas Hofman <klaas.hofman@gmail.com>
2019-09-25 21:14:29 +0200

    end-to-end encoding-decoding works
    low fps solved by adding videorate element, responsible for restamping video frames
    still high delay
    the rtp example is not working

--- video_encoder.py
+++ video_encoder.py
@@ -39,10 +39,12 @@ def main(args):
     # camera caps of internal camera laptop
     VIDEO_RAW_CAPS = 'video/x-raw,format=YUY2,width=640,height=480,framerate=30/1'
     VIDEO_CAPS_RTP = 'application/x-rtp,media=video,encoding-name=H264,payload=98,clock-rate=90000'
-    #VIDEOSRC=''' videotestsrc is-live=true'''
-    VIDEOSRC=''' v4l2src device=/dev/video0 '''
+    VIDEOSRC=''' v4l2src device=/dev/video0 do-timestamp=true ! videorate'''
 
-    RTPSINK = '''udpsink host=localhost port=5000 sync=false'''
+    RTPSINK = '''udpsink host=localhost port=5000 sync=true async=false '''
+    RTPSOURCE = '''udpsrc port=5000'''
+    RTP_PAYLOAD = '''98'''
+
     TEE_RTP = ''' tee name=t '''
     TEE_RAW = ''' tee name=r '''
 
@@ -54,8 +56,19 @@ def main(args):
     # PIPELINE_DESC += ''' r. ! queue ! filesink location=capture.raw '''
     # #PIPELINE_DESC += ''' s. ! queue ! ''' + VIDEO_CAPS_ENC + ''' ! x264enc tune=zerolatency ! h264parse ! rtph264pay ! filesink location=capture.avc '''
     # PIPELINE_DESC += ''' s. ! queue ! videoconvert ! x264enc tune=zerolatency ! h264parse ! rtph264pay ! filesink location=capture.avc '''
-    PIPELINE_DESC = VIDEOSRC + ''' ! ''' + VIDEO_RAW_CAPS + ''' ! videoconvert ! queue ! x264enc ! video/x-h264,profile=baseline ! h264parse ! rtph264pay pt=98 ! ''' + VIDEO_CAPS_RTP + ''' ! queue ! ''' + RTPSINK
-    PIPELINE_DESC += ''' udpsrc port=5000 ! application/x-rtp,media=video,clock-rate=90000,payload=98 ! queue ! rtph264depay ! h264parse !  avdec_h264 ! queue !  videoconvert ! autovideosink'''
+    SRC = VIDEOSRC + ''' ! ''' + VIDEO_RAW_CAPS + ''' ! videoconvert ! '''
+    ENCODER = ''' x264enc ! video/x-h264,profile=baseline ! h264parse ! rtph264pay pt=''' + RTP_PAYLOAD  + ''' ! ''' + VIDEO_CAPS_RTP
+
+    DECODER = ''' application/x-rtp,media=video,clock-rate=90000,payload=''' + RTP_PAYLOAD + ''' ! queue ! rtph264depay ! h264parse !  avdec_h264 '''
+    SINK =  ''' videoconvert ! ximagesink '''
+
+    # without rtp overhead, fps=ok, very high delay > 2 sec
+    PIPELINE_DESC = SRC + ''' queue ! ''' + ENCODER + ''' ! queue ! '''
+    PIPELINE_DESC += DECODER + ''' ! queue ! ''' + SINK
+
+    # transcoding pipeline using RTP (not working currently)
+    PIPELINE_DESC = SRC + ''' queue ! ''' + ENCODER + ''' ! queue ! ''' + RTPSINK
+    PIPELINE_DESC += RTPSOURCE  + ''' ! queue ! ''' + DECODER + ''' ! ''' + SINK
 
     print("pipeline: gst-launch-1.0 " + PIPELINE_DESC)
 
